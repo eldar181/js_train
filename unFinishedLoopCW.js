9927627cbfd72ab5e66d323fe3e9ee29aaaf1faf
Eldar <wrbeta900@gmail.com>
2025-02-04 09:36:14 +0300

    unFinishedLoop.js

---
+++ unFinishedLoopCW.js
@@ -0,0 +1,14 @@
+function createArray(number) {
+  const newArray = [];
+
+  for (let counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
+console.log(createArray(1));
+console.log(createArray(2));
+console.log(createArray(3));
+console.log(createArray(4));
+console.log(createArray(5));
