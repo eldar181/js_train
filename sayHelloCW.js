bedbf3901130b44334b6abeee58608270e861a7c
Eldar <wrbeta900@gmail.com>
2025-02-07 10:31:23 +0300

    sayHelloCW.js

---
+++ sayHelloCW.js
@@ -0,0 +1,9 @@
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+}
+
+console.log(sayHello(["John", "Smith"], "Phoenix", "Arizona"));
+console.log(
+  sayHello(["Franklin", "Delano", "Roosevelt"], "Chicago", "Illinois")
+);
+console.log(sayHello(["Wallace", "Russel", "Osbourne"], "Albany", "New York"));
