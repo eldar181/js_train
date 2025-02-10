c95853f4857447b0c42d97dee1b43f845131d41f
Eldar <wrbeta900@gmail.com>
2025-02-10 10:49:18 +0300

    convertToBinary.js

---
+++ convertToBinaryCW.js
@@ -0,0 +1,8 @@
+function intToBinary(num) {
+  return +num.toString(2);
+}
+
+console.log(intToBinary(1));
+console.log(intToBinary(10));
+console.log(intToBinary(100));
+console.log(intToBinary(1000));
