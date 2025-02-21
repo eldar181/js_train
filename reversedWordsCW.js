796372948f82550e2caf995fe75f2c88476b7e38
Eldar <wrbeta900@gmail.com>
2025-02-21 10:10:27 +0300

    reversedWordsCW.js

---
+++ reversedWordsCW.js
@@ -0,0 +1,6 @@
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+console.log(reverseWords("hello world!"));
+console.log(reverseWords("yoda doesn't speak like this"));
+console.log(reverseWords("kata editor"));
