a72112636f4bfe554887b4eeab8b494be1c44b0a
Eldar <wrbeta900@gmail.com>
2025-02-25 10:08:27 +0300

    schoolScores.js

---
+++ schoolScores.js
@@ -0,0 +1,18 @@
+let score = +prompt();
+switch (score) {
+  case 1:
+    console.log("Bad");
+    break;
+  case 2:
+    console.log("Not bad");
+    break;
+  case 3:
+    console.log("Normal");
+    break;
+  case 4:
+    console.log("Good");
+    break;
+  case 5:
+    console.log("Great");
+    break;
+}
