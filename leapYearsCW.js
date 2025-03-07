7f19f8bc7fea4281140e1ce6e1c4b0bc52d7c455
Eldar <wrbeta900@gmail.com>
2025-03-07 10:11:27 +0300

    leapYearsCW.js

---
+++ leapYearsCW.js
@@ -0,0 +1,8 @@
+function isLeapYear(year) {
+  // TODO
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+console.log(isLeapYear(2020));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(2015));
+console.log(isLeapYear(2100));
